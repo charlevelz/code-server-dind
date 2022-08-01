8ff885f26a896b0e4d0c9611f10601ae72551e51
carlsvelzm <carlsvelz@gmail.com>
2022-08-01 20:09:07 +0000

    initial commit

--- wrapper.sh
+++ wrapper.sh
@@ -3,4 +3,6 @@
 /usr/sbin/sshd
 adduser ${DOCKER_USER-coder} docker
 chown ${DOCKER_USER-coder}: .
-su -c "/usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 ." ${DOCKER_USER-coder}
+
+su -c "--bind-addr 0.0.0.0:8080 ." ${DOCKER_USER-coder}
+# su -c "/usr/local/bin/dind ." ${DOCKER_USER-coder}
